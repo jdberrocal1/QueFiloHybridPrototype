7c7745ac12dabdaf66405b168f7e14b36c54f2fd
Daniel Berrocal <jdanielb1992@gmail.com>
2016-01-31 15:04:09 -0600

    Restaurants

--- QueFilo/www/js/app.js
+++ QueFilo/www/js/app.js
@@ -1,73 +1,72 @@
-// Ionic Starter App
+(function () {
+  'use strict';
+    var app = angular.module('starter', [
+      'ionic',
+      'ngCordova'
+    ]);
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+    app.run(function($ionicPlatform) {
+      $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+          cordova.plugins.Keyboard.disableScroll(true);
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.rest', {
-    url: '/rest',
-    views: {
-      'tab-rest': {
-        templateUrl: 'templates/tab-rest.html',
-        controller: 'restController',
-        controllerAs:'rest'
-      }
-    }
-  })
-
-  .state('tab.orders', {
-      url: '/orders',
-      views: {
-        'tab-orders': {
-          templateUrl: 'templates/tab-orders.html',
-          controller: 'ordersController',
-          controllerAs:'orders'
         }
-      }
+        if (window.StatusBar) {
+          // org.apache.cordova.statusbar required
+          StatusBar.styleDefault();
+        }
+      });
     })
 
-  .state('tab.profile', {
-    url: '/profile',
-    views: {
-      'tab-profile': {
-        templateUrl: 'templates/tab-profile.html',
-        controller: 'profileController',
-        controllerAs:'profile'
-      }
-    }
-  });
+    app.config(function($stateProvider, $urlRouterProvider) {
+      $stateProvider
+      // setup an abstract state for the tabs directive
+        .state('tab', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html'
+      })
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/rest');
+      // Each tab has its own nav history stack:
 
-});
+      .state('tab.rest', {
+        url: '/rest',
+        views: {
+          'tab-rest': {
+            templateUrl: 'templates/tab-rest.html',
+            controller: 'restController',
+            controllerAs:'rest'
+          }
+        }
+      })
+
+      .state('tab.orders', {
+          url: '/orders',
+          views: {
+            'tab-orders': {
+              templateUrl: 'templates/tab-orders.html',
+              controller: 'ordersController',
+              controllerAs:'orders'
+            }
+          }
+        })
+
+      .state('tab.profile', {
+        url: '/profile',
+        views: {
+          'tab-profile': {
+            templateUrl: 'templates/tab-profile.html',
+            controller: 'profileController',
+            controllerAs:'profile'
+          }
+        }
+      });
+
+      // if none of the above states are matched, use this as the fallback
+      $urlRouterProvider.otherwise('/tab/rest');
+
+    });
+})();
--- QueFilo/www/js/services.js
+++ QueFilo/www/js/services.js
@@ -1,50 +1,63 @@
-angular.module('starter.services', [])
-
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
-
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
-
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+(function () {
+  var serviceId = 'RestService';
+  angular.module('starter').service(serviceId, [
+    function (
+    ) {
+      var restaurants=[
+        {
+          name:"Charlies Burgues",
+          tel:"2460-2454",
+          addres:"Costado norte del parque ciudad Quesada San Carlos",
+          description:"Preparamos las hamburguesas mas exquisitas del país, comidas rápidas y platos típicos. Todos nuestros platillos son preparados con materia prima de primera calidad.",
+          schedule:"Lunes-Viernes: 10:00 AM-10:00 PM Sábado-Domingo: 9:00 AM-10:00 PM ",
+          type:"Varios"
+        },
+        {
+          name:"Gran Parrillada",
+          tel:"4701-1818",
+          addres:"100mts norte del Hospital San Carlos",
+          description:"",
+          schedule:" Lunes-Domingo: 10:30 AM-10:00 PM",
+          type:"Varios"
+        },
+        {
+          name:"Kbtos",
+          tel:"2460-2778",
+          addres:"50 mts oeste de la Estacion de Bomberos de Ciudad Quesada",
+          description:"",
+          schedule:"Lunes-Viernes: 8:00 AM-10:00 PM Sábado: 12:00 AM-10:00 PM Domingo: 2:00 PM-09:00 PM ",
+          type:"Varios"
+        },
+        {
+          name:"Kenko Sushi",
+          tel:"2460-8784",
+          addres:"Frente a gasolinera delta en Ciudad Quesada",
+          description:"Especializados en comida Japonesa",
+          schedule:"Lunes-Domingo: 11:30 AM-10:00 PM",
+          type:"Oriental"
+        },
+        {
+          name:"La Forchetta",
+          tel:"2461-1414",
+          addres:"100 mts sur del Liceo San Carlos. Centro Comercial Boreal. Ciudad Quesada",
+          description:"Restauran italiano y pizza a la leña",
+          schedule:"Martes-Viernes: 6:00 PM-9:00 PM Sábado: 12:00 PM-9:00 PM Domingo: 12:00 PM-8:00 PM ",
+          type:"Italiana"
+        },
+        {
+          name:"New York Pizza",
+          tel:"2461-1112",
+          addres:"Barrio San Antonio, costado sur de la Casa del Vidrio.",
+          description:"Un negocio familiar ubicado en barrio San Antonio, Ciudad Quesada especializado en pizzas, pastas y cocina italiana al estilo New York.",
+          schedule:"Lunes-Jueves: 11:00 AM-10:00 PM Viernes-Sábado: 11:00 AM-11:00 PM Domingo: 11:00 AM-10:00 PM ",
+          type:"Italiana"
         }
+      ];
+
+      this.getRestaurants=function getRestaurants(){
+        return restaurants;
       }
-      return null;
+
     }
-  };
-});
+  ]);
+})();
